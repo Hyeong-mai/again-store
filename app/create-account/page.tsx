f75acbe5fbe06b887052588cf675447aecdfc040
Hyeong-mai <phillip1216@naver.com>
2024-04-18 21:00:53 +0900

    2.2 Form Components

--- app/create-account/page.tsx
+++ app/create-account/page.tsx
@@ -1,3 +1,6 @@
+import FormButton from "@/Components/form-btn";
+import FormInput from "@/Components/form-input";
+import SocialLogin from "@/Components/social-login";
 import { ChatBubbleOvalLeftEllipsisIcon } from "@heroicons/react/24/solid";
 import Link from "next/link";
 
@@ -9,16 +12,21 @@ export default function CreateAccount() {
         <h2 className="text-xl">Fill in the form below to join!</h2>
       </div>
       <form className="flex flex-col gap-3">
-        <div className="flex flex-col gap2">
-          <input
-            className="bg-transparent rounded-md w-full h-10 focus:outline-none ring-1 focus:ring-2 ring-neutral-200 focus:ring-orange-500 border-none placeholder:text-neutral-400"
-            type="text"
-            placeholder="Username"
-            required
-          />
-          <span className="text-red-500 font-medium ">Input error</span>
-        </div>
-        <button className="primary-btn h-10">Create account</button>
+        <FormInput type="text" placeholder="Username" required errors={[]} />
+        <FormInput type="email" placeholder="Email" required errors={[]} />
+        <FormInput
+          type="password"
+          placeholder="Password"
+          required
+          errors={[]}
+        />
+        <FormInput
+          type="password"
+          placeholder="Confirm Password"
+          required
+          errors={[]}
+        />
+        <FormButton loading={false} text="Create Account" />
       </form>
       <div className="w-full h-px bg-neutral-500" />
       <div>
@@ -32,6 +40,7 @@ export default function CreateAccount() {
           <span>Sign up with SMS</span>
         </Link>
       </div>
+      <SocialLogin />
     </div>
   );
 }
