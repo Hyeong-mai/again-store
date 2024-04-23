f551092e3f0e5c1b3eb2744ce3c0c18705023d0b
Hyeong-mai <phillip1216@naver.com>
2024-04-23 17:57:42 +0900

    4.7 Coerce

---
+++ app/sms/action.ts
@@ -0,0 +1,10 @@
+"use server";
+
+import validator from "validator";
+import { z } from "zod";
+
+const phoneSchema = z.string().trim().refine(validator.isMobilePhone);
+
+const tokenSchema = z.coerce.number().min(100000).max(999999);
+
+export async function smsLogIn(prevState: any, formData: FormData) {}
