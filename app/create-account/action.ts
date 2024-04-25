5a3610608b23ac036907e29fe91c3da05de8d15b
Hyeong-mai <phillip1216@naver.com>
2024-04-25 15:34:55 +0900

    6.1 Database Validation

--- app/create-account/action.ts
+++ app/create-account/action.ts
@@ -1,4 +1,5 @@
 "use server";
+import db from "@/lib/db";
 import { z } from "zod";
 
 const passwordRegex = new RegExp(
@@ -15,6 +16,35 @@ const checkPassword = ({
   confirmPassword: string;
 }) => password === confirmPassword;
 
+const checkUniqueUsername = async (username: string) => {
+  const user = await db.user.findUnique({
+    where: {
+      username,
+    },
+    select: {
+      id: true,
+    },
+  });
+  // if (user) {
+  //   return false;
+  // } else {
+  //   return true;
+  // }
+  return !Boolean(user);
+};
+
+const checkUniqueEmail = async (email: string) => {
+  const user = await db.user.findUnique({
+    where: {
+      email,
+    },
+    select: {
+      id: true,
+    },
+  });
+  return !Boolean(user);
+};
+
 const formSchema = z
   .object({
     username: z
@@ -26,8 +56,17 @@ const formSchema = z
       .max(10, "That is too long!")
       .trim()
       .toLowerCase()
-      .refine(checkUsername, "No potato allowed!"),
-    email: z.string().email().trim().toLowerCase(),
+      .refine(checkUsername, "No potato allowed!")
+      .refine(checkUniqueUsername, "This username is already taken"),
+    email: z
+      .string()
+      .email()
+      .trim()
+      .toLowerCase()
+      .refine(
+        checkUniqueEmail,
+        "There id an already registered with that email"
+      ),
     password: z
       .string()
       .min(10)
@@ -52,10 +91,9 @@ export default async function createAccount(
     password: formData.get("password"),
     confirmPassword: formData.get("confirmPassword"),
   };
-  const result = formSchema.safeParse(data);
+  const result = await formSchema.safeParseAsync(data);
   if (!result.success) {
     return result.error.flatten();
   } else {
-    console.log(result.data);
   }
 }
